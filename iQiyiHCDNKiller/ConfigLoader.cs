1240ad4634c8bf88bd9737fe0e243c445c8c00d0
fengzhe <1k.magnus@gmail.com>
2015-09-22 14:19:17 +0800

    first commit

---
+++ iQiyiHCDNKiller/ConfigLoader.cs
@@ -0,0 +1,49 @@
+﻿namespace iQiyiHCDNKiller
+{
+    public class ConfigLoader
+    {
+        public int waitexit = 3000;
+        public string clientpath = @"C:\Program Files (x86)\IQIYI Video\PStyle";
+        public int waitstart = 3000;
+
+        private static ConfigLoader _instance = new ConfigLoader();
+        public bool inited = false;
+
+        public static ConfigLoader instance
+        {
+            get
+            {
+                _instance.init();
+                return _instance;
+            }
+        }
+
+        private ConfigLoader()
+        {
+        }
+
+        private void init()
+        {
+            if (!inited)
+            {
+                string inifile = System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "config.ini");
+                if (System.IO.File.Exists("config.ini"))
+                {
+                    IniHelper ih = new IniHelper(inifile);
+                    clientpath = ih.ReadValue("Path", "QyClient");
+                    string strstart = ih.ReadValue("Config", "WaitStart");
+                    waitstart = int.Parse(strstart);
+                    waitexit = int.Parse(ih.ReadValue("Config", "WaitExit"));
+                }
+                else
+                {
+                    IniHelper ih = new IniHelper(inifile);
+                    ih.WriteValue("Path", "QyClient", clientpath);
+                    ih.WriteValue("Config", "WaitStart", waitstart.ToString());
+                    ih.WriteValue("Config", "WaitExit", waitexit.ToString());
+                }
+                inited = true;
+            }
+        }
+    }
+}
---
+++ iQiyiHCDNKiller/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iQiyiHCDNKiller
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            runclient();
+            //Application.Run(new Form1());
+            Application.Run();
+        }
+
+        static void runclient()
+        {
+            ConfigLoader cl = ConfigLoader.instance;
+            System.Diagnostics.Process exep = new System.Diagnostics.Process();
+            exep.StartInfo.FileName = Path.Combine(cl.clientpath, "QyClient.exe");
+            exep.Start();
+            exep.WaitForExit();
+            System.Threading.Thread.Sleep(cl.waitstart);
+            System.Diagnostics.Process[] procarray = System.Diagnostics.Process.GetProcessesByName("QyClient");
+            if (procarray.Length == 0)
+            {
+                exit_kernal();
+                Application.Exit();
+            }
+            foreach (System.Diagnostics.Process proc in procarray)
+            {
+                proc.EnableRaisingEvents = true;
+                proc.Exited += new EventHandler(exep_Exited);
+            }
+        }
+
+        //exep_Exited事件处理代码，这里外部程序退出后激活，可以执行你要的操作
+        static void exep_Exited(object sender, EventArgs e)
+        {
+            exit_kernal();
+        }
+
+        static void exit_kernal()
+        {
+            ConfigLoader cl = ConfigLoader.instance;
+            System.Threading.Thread.Sleep(cl.waitexit);
+            System.Diagnostics.Process[] procarray = System.Diagnostics.Process.GetProcessesByName("QyKernel");
+            foreach (System.Diagnostics.Process proc in procarray)
+            {
+                proc.Kill();
+            }
+            Application.Exit();
+        }
+    }
+}
